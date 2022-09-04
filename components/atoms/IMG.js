0df73cd9dfd80110af5a8b8fdd835b50a54c7517
Łukasz Brzózko <lukasz9099@gmail.com>
2022-09-04 17:31:22 +0200

    feat: add IMG component

---
+++ components/atoms/IMG.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { getStrapiMedia } from "../../lib/media";
+
+function IMG({ image, objectFit }) {
+  const { alternativeText, width, height } = image.data.attributes;
+
+  return (
+    <Image
+      layout="fill"
+      width={width}
+      height={height}
+      objectFit={objectFit || "cover"}
+      src={getStrapiMedia(image)}
+      alt={alternativeText || ""}
+    />
+  );
+}
+
+export default IMG;
