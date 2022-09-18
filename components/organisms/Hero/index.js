08141f011f24b48f2402edcc4fbb84fe322c0151
Łukasz Brzózko <lukasz9099@gmail.com>
2022-09-18 17:29:37 +0200

    feat: add framer motion

--- components/organisms/Hero/index.js
+++ components/organisms/Hero/index.js
@@ -1,6 +1,7 @@
 import block from "bem-css-modules";
 import { useCallback, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 import IMG from "../../atoms/IMG";
 import Overlay from "../../atoms/Overlay";
@@ -15,8 +16,33 @@ const CONSTANTS = {
   scrollTranslateRatio: 0.1,
 };
 
+const VARIANTS_NAMES = {
+  visible: "visible",
+  hidden: "hidden",
+};
+
+const variants = {
+  [VARIANTS_NAMES.visible]: (i = 0) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: i * 0.1,
+      staggerChildren: 0.1,
+    },
+  }),
+  [VARIANTS_NAMES.hidden]: { y: 50, opacity: 0 },
+};
+
 function Hero({ data }) {
-  const { overlay, imgDesktop, imgMobile, title, subtitle } = data;
+  const {
+    overlay,
+    imgDesktop,
+    imgMobile,
+    titleLineOne,
+    titleLineTwo,
+    titleLineThree,
+    subtitle,
+  } = data;
 
   const [imgYPosition, setImgYPosition] = useState(0);
 
@@ -67,19 +93,48 @@ function Hero({ data }) {
         <Grid>
           <div className={b("text-block")}>
             {/* Subtitle */}
-            <span
+            <motion.div
+              custom={0}
+              initial={VARIANTS_NAMES.hidden}
+              animate={VARIANTS_NAMES.visible}
+              variants={variants}
               className={`${b(
                 "subtitle"
               )} t-typo-caption ui-block ui-color--t-secondary`}
             >
               {subtitle}
-            </span>
+            </motion.div>
 
             {/* Title */}
-            <div
-              className={`${b("title")} t-typo-h1`}
-              dangerouslySetInnerHTML={{ __html: title }}
-            ></div>
+            <div className={`${b("title")} t-typo-h1`}>
+              <motion.span
+                custom={1}
+                initial={VARIANTS_NAMES.hidden}
+                animate={VARIANTS_NAMES.visible}
+                variants={variants}
+                className="ui-block"
+              >
+                {titleLineOne}
+              </motion.span>
+              <motion.h1
+                custom={2}
+                initial={VARIANTS_NAMES.hidden}
+                animate={VARIANTS_NAMES.visible}
+                variants={variants}
+                className="ui-color--secondary"
+              >
+                {titleLineTwo}
+              </motion.h1>
+              <motion.h2
+                custom={3}
+                initial={VARIANTS_NAMES.hidden}
+                animate={VARIANTS_NAMES.visible}
+                variants={variants}
+                className="t-typo-h2"
+              >
+                {titleLineThree}
+              </motion.h2>
+            </div>
           </div>
         </Grid>
       </Inner>
