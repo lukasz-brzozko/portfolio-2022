22211f44155d2f59d3d85141c592e28e39d44e8d
Łukasz Brzózko <lukasz9099@gmail.com>
2022-10-01 19:59:29 +0200

    feat: use forwardRef for Inner component

--- components/layout/Inner/index.js
+++ components/layout/Inner/index.js
@@ -1,13 +1,20 @@
 import block from "bem-css-modules";
+import { forwardRef } from "react";
 
 import styles from "./Inner.module.scss";
 
 const b = block(styles);
 
-function Inner({ children, className }) {
+const Inner = forwardRef(({ children, className }, ref) => {
   const classNameText = className ? ` ${className}` : "";
 
-  return <div className={`${b()}${classNameText}`}>{children}</div>;
-}
+  return (
+    <div ref={ref} className={`${b()}${classNameText}`}>
+      {children}
+    </div>
+  );
+});
+
+Inner.displayName = "Inner";
 
 export default Inner;
