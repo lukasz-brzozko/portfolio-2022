4208b2105fb4f25580af1e56775286b06775243e
Łukasz Brzózko <lukasz9099@gmail.com>
2022-10-11 16:56:53 +0200

    feat: add modal (wip)

--- components/molecules/Portal/index.js
+++ components/molecules/Portal/index.js
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { createPortal } from "react-dom";
 
 import { ProjectContext } from "../../../contexts/ProjectContext";
 
 function Portal({ children }) {
+  const { selectedProjectID } = useContext(ProjectContext);
+
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true);
+    if (selectedProjectID) {
+      setMounted(true);
+    }
 
     return () => setMounted(false);
-  }, []);
+  }, [selectedProjectID]);
 
   return mounted
     ? createPortal(children, document.querySelector("#portal"))
--- components/molecules/ProjectModal/index.js
+++ components/molecules/ProjectModal/index.js
@@ -1,15 +1,46 @@
 import block from "bem-css-modules";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+
+import { ProjectContext } from "../../../contexts/ProjectContext";
+import IMG from "../../atoms/IMG";
 
 import styles from "./ProjectModal.module.scss";
 
 const b = block(styles);
 
-function ProjectModal() {
+function ProjectModal({ projects }) {
+  const { selectedProjectID, setSelectedProjectID } =
+    useContext(ProjectContext);
+
+  const [highlightedProject, setHighlightedProject] = useState(null);
+
+  useEffect(() => {
+    const chosenProject = projects.find(
+      (project) => project.id === selectedProjectID
+    );
+
+    console.log(chosenProject);
+
+    setHighlightedProject(chosenProject);
+  }, [selectedProjectID, projects]);
+
   return (
     <AnimatePresence>
-      <motion.div className={b()}>ProjectModal</motion.div>
+      {highlightedProject && (
+        <motion.div
+          key={selectedProjectID}
+          layoutId={selectedProjectID}
+          className={`${b()}`}
+          onClick={() => {
+            setSelectedProjectID(null);
+          }}
+        >
+          <motion.div>{highlightedProject?.attributes.title}</motion.div>
+          <IMG image={highlightedProject?.attributes.img} />
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
