6388c25b95e5a4afe0016dcf0c0d943226a045ca
Łukasz Brzózko <lukasz9099@gmail.com>
2022-12-27 19:17:40 +0100

    Deploy

---
+++ out/_next/static/1jQV5zb_JcPvaO2FVVO3X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/816-dcbb7f0f8f8ef74a.js","static/css/c10b0de29ba82c96.css","static/chunks/pages/index-92d4d511ea860ea9.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
