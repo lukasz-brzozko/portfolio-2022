2b5180842aa02c0bbfa65e7e261bb73b0fea92b5
Łukasz Brzózko <lukasz9099@gmail.com>
2022-12-28 12:05:00 +0100

    config: add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import "./firebase";
+
 import PageHead from "../components/organisms/PageHead";
+
 import "../styles/base/cssVars/_index.scss";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <PageHead />
       <Component {...pageProps} />
       <div id="portal"></div>
     </>
---
+++ pages/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUwkpEyp75zlgRMZibVCGKtxZtoLz_hvA",
+  authDomain: "lbrzozko-2022.firebaseapp.com",
+  projectId: "lbrzozko-2022",
+  storageBucket: "lbrzozko-2022.appspot.com",
+  messagingSenderId: "402678408733",
+  appId: "1:402678408733:web:afed50e79ecc82884488f4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
